8287575e31a512970fe38e3650b8112a2925b129
Dektg <56469979+Dektg@users.noreply.github.com>
2020-01-03 16:59:01 +1000

    Create Akinator.py

---
+++ Akinator.py
@@ -0,0 +1,26 @@
+import random as rand
+import time
+
+print("Добрый день, сер.")
+time.sleep(1)
+print("Я загадал число от 100 до 1.\nПопробуй отгадать!")
+
+namber = rand.randint(1, 101)
+guess = input()
+guess = int(guess)
+Guess_Of_namber: int = 0
+
+while namber != guess:
+    if guess > namber:
+        print("Слишком много!")
+        guess = input()
+        guess = int(guess)
+        Guess_Of_namber += 1
+
+    if guess < namber:
+        print("Слишком мало!")
+        guess = input()
+        guess = int(guess)
+        Guess_Of_namber += 1
+
+print("Ты угадал!\nЗа: ", Guess_Of_namber, " попыток.")
